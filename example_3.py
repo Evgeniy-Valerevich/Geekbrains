bdd9ab1a76e8abff68db94adfe6881e57a35d939
Evgeniy-Valerevich <56393330+Evgeniy-Valerevich@users.noreply.github.com>
2022-11-17 21:05:01 +0500

    Delete example_3.py

--- example_3.py
+++
@@ -1,5 +0,0 @@
-number_1 = input("enter a number: ")
-number_2 = number_1 + number_1
-number_3 = number_1 + number_1 + number_1
-final_number = int(number_1) + int(number_2) + int(number_3)
-print(f'The final number: {final_number}')
